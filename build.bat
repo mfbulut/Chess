822b77d2c64482580364f8979a2b4da2e0e1cbdb
Mustafa Furkan Bulut <mfbulut2006@gmail.com>
2025-09-27 23:32:09 +0300

    Reorganize code

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 set LIB="C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um/x64/"
-zig c++ ./src/main.cpp ./src/tpng.c ./assets/resource.res -L %LIB% -ld3d11 -ld3dcompiler -ldwmapi -lole32 -o .\build\chess.exe
+zig c++ ./src/main.cpp ./src/tpng.c ./assets/resource.res -L %LIB% -ld3d11 -ld3dcompiler -ldwmapi -lole32 -Oz -s -fno-exceptions -fno-rtti -ffunction-sections -fdata-sections -Wl,--gc-sections,--subsystem,windows -o chess.exe
